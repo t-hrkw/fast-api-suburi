6a7c7c4b73eca77a5a7f79be88242526f4a1e041
t_hrkw <tomoyasu156045@gmail.com>
2021-09-14 18:10:23 +0900

    クエリパラメータの素振り（デフォルト値や必須ではないパラメータの設定など）

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
 
 
-@app.get('/')
-def index():
-  return {'data': 'blog list'}
+@app.get('/blog')
+def index(limit=10, published: bool = True, sort: Optional[str] = None):
+  # only get 10 published blogs
+  if published:
+    return {'data': f'{limit} published blogs from the db' }
+  else:
+    return {'data': f'{limit} blogs from the db'}
 
 # これを@app.get('/blog/{id}')より先に書かないといけない
 @app.get('/blog/unpublished')
@@ -18,5 +23,6 @@ def show(id: int):
 
 
 @app.get('/blog/{id}/comments')
-def comments(id: int):
+def comments(id, limit = 10):
+  return limit
   return {'data': {'1', '2'}}
