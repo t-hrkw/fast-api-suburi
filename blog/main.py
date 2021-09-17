2164860234b708af7ccb9aa77f8ae5399eef89e1
t_hrkw <tomoyasu156045@gmail.com>
2021-09-17 11:05:04 +0900

    Hash Password(2)

--- blog/main.py
+++ blog/main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI, Depends, status, Response, HTTPException
 from . import schemas, models
 from .database import engine, SessionLocal
 from sqlalchemy.orm import Session
-from passlib.context import CryptContext
+from .hashing import Hash
 
 app = FastAPI()
 
@@ -61,12 +61,9 @@ def show(id, response: Response, db: Session = Depends(get_db)):
   return blog
 
 
-pwd_cxt = CryptContext(schemes = ["bcrypt"], deprecated = "auto")
-
 @app.post('/user')
 def create_user(request: schemas.User, db: Session = Depends(get_db)):
-  hashedPassword = pwd_cxt.hash(request.password)
-  new_user = models.User(name = request.name, email = request.email, password = hashedPassword)
+  new_user = models.User(name = request.name, email = request.email, password = Hash.bcrypt(request.password))
   db.add(new_user)
   db.commit()
   db.refresh(new_user)
