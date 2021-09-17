fa3b39cad4ae3c01ef29becd1a6204f0a6d092df
t_hrkw <tomoyasu156045@gmail.com>
2021-09-17 17:01:10 +0900

    blogとuserの多対多の関係?(Relationship)

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,16 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.sql.schema import ForeignKey
 from .database import Base
+from sqlalchemy.orm import relationship
 
 class Blog(Base):
   __tablename__ = 'blogs'
   id = Column(Integer, primary_key = True, index = True)
   title = Column(String)
   body = Column(String)
+  user_id = Column(Integer, ForeignKey('users.id'))
+
+  creator = relationship("User", back_populates = "blogs")
 
 
 class User(Base):
@@ -14,3 +19,5 @@ class User(Base):
   name = Column(String)
   email = Column(String)
   password = Column(String)
+
+  blogs = relationship('Blog', back_populates = "creator")
--- blog/schemas.py
+++ blog/schemas.py
@@ -1,12 +1,11 @@
+from typing import List
 from pydantic import BaseModel
 
-class Blog(BaseModel):
+class BlogBase(BaseModel):
   title: str
   body: str
 
-class ShowBlog(BaseModel):
-  title: str
-  body: str
+class Blog(BlogBase):
   class Config():
     orm_mode = True
 
@@ -14,13 +13,21 @@ class ShowBlog(BaseModel):
 class User(BaseModel):
   name: str
   email: str
-  password: str
 
 
 class ShowUser(BaseModel):
   name: str
   email: str
-  password: str
+  blogs: List[Blog] = []
+
+  class Config():
+    orm_mode = True
+
+
+class ShowBlog(BaseModel):
+  title: str
+  body: str
+  creator: ShowUser
 
   class Config():
     orm_mode = True
