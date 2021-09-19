c01a39b154f4d85c98d861795c37ad32240ad509
t_hrkw <tomoyasu156045@gmail.com>
2021-09-19 19:59:45 +0900

    JWT Access Token

--- blog/routers/authentication.py
+++ blog/routers/authentication.py
@@ -1,6 +1,5 @@
-
 from fastapi import APIRouter, Depends, status, HTTPException
-from .. import schemas, database, models
+from .. import schemas, database, models, token
 from ..hashing import Hash
 from sqlalchemy.orm import Session
 
@@ -16,5 +15,6 @@ def login(request: schemas.Login, db: Session = Depends(database.get_db)):
     raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail = f"Invalid Credentials")
   if not Hash.verify(request.password, user.password):
     raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, detail = f"Incorrect email or password")
-  # generate a jwt token and return
-  return user
+
+  access_token = token.create_access_token(data = {"sub": user.email})
+  return {"access_token": access_token, "token_type": "bearer"}
